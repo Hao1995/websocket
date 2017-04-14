d8ad8af912dd1ad8a45175ae849f254047c5e471
unknown <>
2017-04-14 11:41:58 +0800

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+var app = require('express')(),
+    http = require("http").Server(app),
+    io = require('socket.io')(http),
+    port = 3000;
+
+app.get('/', function (req, res) {
+    res.send("TEST");
+});
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
+
+http.listen(port, function () {
+    console.log("Server Started!!!");
+})
