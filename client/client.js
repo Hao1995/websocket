18608011154c3f8e64898c77a90b2eb2efa4c1c6
root <y26704325@gmail.com>
2017-04-28 18:09:21 +0800

    complete-1 >> delete unneeded floder

--- client/client.js
+++
@@ -1,7 +0,0 @@
-var socket = io.connect('http://localhost:3000');
-socket.emit('my other event', { my: 'data' });
-socket.on('news', function (data) {
-    var img = document.getElementById("photo1");
-    console.log("Client : "+ data);
-    // socket.emit('my other event', { my: 'data' });
-});
